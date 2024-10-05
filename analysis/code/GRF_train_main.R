865a951b9aa101165d495abb5a323a5bd8153894
nickmlacoste <nickmlacoste@gmail.com>
2024-10-04 20:16:42 -0500

    Created skeleton code for training and saving the multi-outcome causal forest. At thing point I have not yet added any code for data visualization or results exporting (beyond saving the CF model)

--- analysis/code/GRF_train_main.R
+++ analysis/code/GRF_train_main.R
@@ -17,11 +17,37 @@ output_path <- "C:/Users/nickm/OneDrive/Acer (new laptop)/Documents/PhD/Tulane U
 cf_output_path <- "C:/Users/nickm/OneDrive/Acer (new laptop)/Documents/PhD/Tulane University/Projects/Pakistan Audits/Large_Output"
 
 # Import 2013-2014 tax returns 
-tax_returns_13.14 <- read_dta(file.path(data_path, "Individual_ITReturns_2013_2014_cleaned.dta"))
+tax_returns_df <- read.csv(file.path(data_path, "tax_returns_data_ML_cleaned.csv"))
+
+# Estimate multi-arm causal forest --------------------------
+
+X_covariates <- c("taxableinc_2014", "incfrsalary_2014", "incfrbusiness_2014")
+
+# remove rows with missing data for Y or W
+tax_returns_df <- tax_returns_df %>%
+  filter(!is.na(audited_2014) & 
+           !is.na(NPV_taxrevenue_2014) & 
+           !is.na(audit_cost_2014) &
+           !is.na(burden_2014))
+
+# need to define the training objects as matrices/vectors
+X_matrix <- as.matrix(tax_returns_df[, X_covariates])
+Y_matrix <- as.matrix(tax_returns_df[, c("NPV_taxrevenue_2014", "audit_cost_2014", "burden_2014")])
+W_vector <- factor(as.vector(tax_returns_df$audited_2014))
 
 
+# train GRF
 
+cf_model <- multi_arm_causal_forest(X = X_matrix,
+                                    Y = Y_matrix,
+                                    W = W_vector,
+                                    Y.hat = NULL,
+                                    W.hat = NULL,
+                                    num.trees = 1000
+                                    )
 
+# Save the causal forest model
+saveRDS(cf_model, file = paste0(cf_output_path, "/cf_model_trained.rda"))
 
 
 
